578f9de38aa93c32bca4e68d3a320c06f8de9bcf
Zuji Zhou <z.zhou-38@student.tudelft.nl>
2025-05-21 13:44:06 +0200

    debug

--- pylint_ml_smells.py
+++ pylint_ml_smells.py
@@ -1,29 +1,31 @@
+# File: pylint_ml_smells.py
+import astroid
 from pylint.checkers import BaseChecker
-from pylint.checkers.utils import check_messages
+from pylint.interfaces import IAstroidChecker
 
 class MLCodeSmellChecker(BaseChecker):
+    __implements__ = IAstroidChecker
+
     name = 'ml-code-smell-checker'
     priority = -1
     msgs = {
         'C9001': (
-            'Model training and prediction are tightly coupled (fit() followed by predict() in same function)',
+            'Model training and prediction are tightly coupled (fit() followed by predict() in the same function)',
             'ml-training-predict-coupling',
-            'Avoid calling fit() and predict() in the same function; separate training and inference.'
+            'Avoid calling fit() and predict() in the same function. Separate training and inference logic.'
         ),
     }
 
     def visit_functiondef(self, node):
-        fit_found = False
-        predict_found = False
-        for child in node.body:
-            if hasattr(child, 'as_string'):
-                line = child.as_string()
-                if '.fit(' in line:
-                    fit_found = True
-                if '.predict(' in line and fit_found:
-                    predict_found = True
-        if fit_found and predict_found:
-            self.add_message('C9001', node=node)
+        """Detects fit() followed by predict() within a single function body"""
+        saw_fit = False
+        for call in node.nodes_of_class(astroid.nodes.Call):
+            func_expr = call.func.as_string()
+            if func_expr.endswith('.fit'):
+                saw_fit = True
+            if saw_fit and func_expr.endswith('.predict'):
+                self.add_message('C9001', node=node)
+                break
 
 def register(linter):
     linter.register_checker(MLCodeSmellChecker(linter))
