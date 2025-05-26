39e899165e83f100fab984870fd8e04eadbba80d
Anna Visman <a.c.visman@student.tudelft.nl>
2025-05-26 18:46:16 +0200

    fix to export testing libs in package data for model-training to reference

--- setup.py
+++ setup.py
@@ -13,4 +13,8 @@ setup(
         "joblib~=1.4.2", 
     ],
     python_requires='>=3.10',
+    include_package_data=True,
+    package_data={
+        'lib_ml': ['.flake8', '.pylintrc'],  # or move these files into lib_ml/
+    }
 )
